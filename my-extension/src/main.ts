cac9a6cdc165fe97edb410d38fc6fa1e207402ef
JohnAnon9771 <njoao97710@gmail.com>
2021-07-12 16:29:39 -0300

    :tada: finish logic and project

--- my-extension/src/main.ts
+++ my-extension/src/main.ts
@@ -1,8 +1,34 @@
 import './style.css'
+import {api} from './services'
 
-const app = document.getElementById('#app') as HTMLDivElement
+const formElement = document.querySelector('.form-data') as HTMLFormElement
+const regionElement = document.getElementById('region') as HTMLInputElement
+const submitButton = document.querySelector('.search-btn') as HTMLButtonElement
 
-app.innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`
+const resultElement = document.querySelector('.result') as HTMLDivElement
+const myRegionElement = document.querySelector('.my-region') as HTMLSpanElement
+const carbonElement = document.querySelector('.carbon-usage') as HTMLSpanElement
+const fossilElement = document.querySelector('.fossil-fuel') as HTMLSpanElement
+const clearButton = document.querySelector('.clear-btn') as HTMLButtonElement
+
+resultElement.hidden = true
+
+submitButton.onclick = async e => {
+	e.preventDefault()
+	const result = await api.get('', {params: {countryCode: regionElement.value}})
+	if (result.data) {
+		formElement.hidden = true
+		resultElement.hidden = false
+
+		myRegionElement.textContent = result.data.countryCode
+		carbonElement.textContent = result.data.data.carbonIntensity
+		fossilElement.textContent = result.data.data.fossilFuelPercentage
+	}
+}
+
+clearButton.onclick = e => {
+	e.preventDefault()
+	formElement.reset()
+	formElement.hidden = false
+	resultElement.hidden = true
+}
